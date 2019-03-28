8c92725867296accc157bbd74e2e3a63b7d6c9cc
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-03-28 14:16:09 +0100

    started work of reducer with email changed

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,9 +1,13 @@
 //reducer for handling email and password authentication
 //initial state for work with app
+import { EMAIL_CHANGED } from '../actions/types';
+
 const INITIAL_STATE = { email: ''};
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
+        case EMAIL_CHANGED:
+
         default:
             return state;
     }
