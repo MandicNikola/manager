d9129ede00ff52b9056b49213a2aabbe1b67a8a7
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-03-28 13:13:55 +0100

    Connecting with firebase

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import { View, Text } from 'react-native';
 import { createStore } from 'redux';
-import reducers from './reducers'
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyAWn5WsgEX_iUOR4gsBVFrBUw-mtmNrXdQ',
+            authDomain: 'manager-33dfe.firebaseapp.com',
+            databaseURL: 'https://manager-33dfe.firebaseio.com',
+            projectId: 'manager-33dfe',
+            storageBucket: 'manager-33dfe.appspot.com',
+            messagingSenderId: '158928662016'
+          };
+          
+          firebase.initializeApp(config);
+    }
+
+
 
     render()
     {
