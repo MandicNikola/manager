b3aa719f488c115a47bef1f890dce3bd5d7cbf71
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-03-28 14:04:52 +0100

    added actions

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,11 @@
+//reducer for handling email and password authentication
+//initial state for work with app
+const INITIAL_STATE = { email: ''};
+
+export default (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+
+};
