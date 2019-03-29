ed64bc85b693612d5615419dd557f066fda009a4
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-03-29 16:01:32 +0100

    reducer to work with pass changed state

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,13 +1,22 @@
 //reducer for handling email and password authentication
 //initial state for work with app
-import { EMAIL_CHANGED } from '../actions/types';
+import { EMAIL_CHANGED,
+     PASSWORD_CHANGED
+} from '../actions/types';
 
-const INITIAL_STATE = { email: ''};
+const INITIAL_STATE =
+ { email: '',
+  password : ''
+};
 
+
+//returning new state object that components can render again
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case EMAIL_CHANGED:
-
+            return {...state, email:action.payload };
+        case PASSWORD_CHANGED:
+            return {...state, password:action.payload }; 
         default:
             return state;
     }
