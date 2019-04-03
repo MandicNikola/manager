132600b72b4599d2a79acb300e3834a8ad68f58a
MandicNikola <mandic_nikola96@uns.ac.rs>
2019-04-03 17:27:57 +0200

    added methods for rendering EmployeeList component

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,10 +1,39 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { ListView, View, Text } from 'react-native';
+import { employeesFetch } from '../actions';
+
+
 
 class EmployeeList extends Component {
 
+    componentWillMount()
+    {
+        this.props.employeesFetch();
+
+        this.createDataSource(this.props);
+    }
+
+    componentWillReceiveProps(nextProps)
+    {
+        //nextProps next set of props taht this component will be rendered
+        //this.props still old set of props
+        this.createDataSource(nextProps);
+    }
+
+    createDataSource({ employees })
+    {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1,r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(this.props.employees);
+    }
+
     render()
     {
+        console.log(this.props);
         return(
             <View>
                 <Text>Employee List!</Text>
@@ -20,4 +49,13 @@ class EmployeeList extends Component {
 
 }
 
-export default EmployeeList;
+const mapStateToProps = state => {
+    const employees = _.map(state.employees, (val, uid) => {
+        return {...val, uid};
+    });
+
+    return {employees};
+};
+
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
